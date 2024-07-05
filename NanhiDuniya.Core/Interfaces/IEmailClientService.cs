6e9d578ef71c99cf45be45bac2fa6c938ad3de46
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-07-05 18:23:00 +0530

    changes inside emailclientservice

--- NanhiDuniya.Core/Interfaces/IEmailClientService.cs
+++ NanhiDuniya.Core/Interfaces/IEmailClientService.cs
@@ -9,7 +9,7 @@ namespace NanhiDuniya.Core.Interfaces
 {
     public  interface IEmailClientService
     {
-        Task<bool> SendEmailAsync(string subject,string firstname,string resetLink, string? htmlBody = null, string? plainTextBody = null, string? templateName = null, string? to = null, List<string>? toList = null);
+        Task<bool> SendEmailAsync(string subject, string firstname, string resetLink, string? htmlBody = null, string? plainTextBody = null, string? templateName = null, string? to = null, List<string>? toList = null);
         Task<bool> SendEmailAsync(NanhiDuniyaEmailRequest request);
     }
 }
