8be813ee583c986ec4976f85f0cc3ca73143d40d
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-07-11 00:16:09 +0530

    Reset password with validation

--- NanhiDuniya-Frontend/nanhiduniya-client/src/app/auth/resetpassword/page.tsx
+++ NanhiDuniya-Frontend/nanhiduniya-client/src/app/auth/resetpassword/page.tsx
@@ -7,17 +7,25 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { toast, ToastContainer } from '../../../components/Shared/ToastMessage/ToastContainer';
 import UserService from '@/services/UserManagement/UserService';
 import ResetPassword from '@/model/User';
+import { FieldValues, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ResetPasswordFormValidation } from '@/lib/validation';
 
 const ResetPasswordPage = () => {
   const searchParams = useSearchParams();
   const token = searchParams?.get('token') || '';
   const email = searchParams?.get('email') || '';
   const router = useRouter();
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  // const [newPassword, setNewPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
   // const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+
+  const { register, handleSubmit, formState: { errors }, reset, } = useForm({
+    resolver: zodResolver(ResetPasswordFormValidation),
+  });
 
   useEffect(() => {
     if (!token || !email) {
@@ -25,22 +33,21 @@ const ResetPasswordPage = () => {
     }
   }, [token, email]);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    debugger
-    e.preventDefault();
-    if (newPassword !== confirmPassword) {
+  const onSubmit = async (data: FieldValues) => {
+    if (data.newPassword !== data.confirmPassword) {
       toast.error('Passwords do not match.');
       return;
     }
-    const resetPasswordData: ResetPassword = { email, token, newPassword };
+    const resetPasswordData: ResetPassword = { email, token, newPassword: data.newPassword };
     try {
       const response = await UserService.resetPassword(resetPasswordData);
-
-      if (response.status) {
-        debugger
+      if (response.statusCode===200) {
         toast.success('Password reset successful.');
+        reset();
+        setTimeout(() => {
+          router.push('/auth/login');
+      }, 3000);
       } else {
-        debugger
         const data = await response.json();
         toast.error(data.error || 'Error resetting password.');
       }
@@ -57,18 +64,20 @@ const ResetPasswordPage = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
         <h2 className="text-2xl font-bold text-center">Reset Password</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4 relative">
             <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">New Password</label>
             <input
               type={showPassword ? 'text' : 'password'} // Toggle input type
+              {...register("newPassword")}
               id="newPassword"
               name="newPassword"
               placeholder="Enter new password"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
+              className={`mt-1 block w-full px-3 py-2 border ${errors.newPassword ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
+            {
+              errors?.newPassword && (<span className="text-red-500 text-sm mt-1">{`${errors.newPassword.message}`}</span>)
+            }
             <button
               type="button"
               onClick={togglePasswordVisibility}
@@ -81,13 +90,15 @@ const ResetPasswordPage = () => {
             <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
             <input
               type={showPassword ? 'text' : 'password'} // Toggle input type
+              {...register("confirmPassword")}
               id="confirmPassword"
               name="confirmPassword"
               placeholder="Confirm password"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
+              className={`mt-1 block w-full px-3 py-2 border ${errors.confirmPassword ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
+            {
+              errors?.confirmPassword && (<span className='text-red-500 text-sm mt-1'>{`${errors.confirmPassword.message}`}</span>)
+            }
             <button
               type="button"
               onClick={togglePasswordVisibility}
--- NanhiDuniya-Frontend/nanhiduniya-client/src/lib/validation.ts
+++ NanhiDuniya-Frontend/nanhiduniya-client/src/lib/validation.ts
@@ -1,4 +1,22 @@
-// import { z } from "zod";
+import { z } from "zod";
+
+export const ResetPasswordFormValidation = z.object({
+    newPassword: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character" }),
+
+    confirmPassword: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character" }),
+});
 
 // export const UserFormValidation = z.object({
 //   name: z
